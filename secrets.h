137b48f272c406c11cf290991d35c0b5c461f93b
Agam <166210205+aagamagrawal@users.noreply.github.com>
2024-04-07 14:47:31 +1000

    Create secrets.h

---
+++ secrets.h
@@ -0,0 +1,8 @@
+// Use this file to store all of the private credentials 
+// and connection details
+
+#define SECRET_SSID "Agam's iPhone"		// replace MySSID with your WiFi network name
+#define SECRET_PASS "AgamAgarwal"	// replace MyPassword with your WiFi password
+
+#define SECRET_CH_ID 2498085			// replace 0000000 with your channel number
+#define SECRET_WRITE_APIKEY "KR34INA744JYRP57"   // replace XYZ with your channel write API Key
